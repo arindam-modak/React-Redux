63bf25a1b47a45decf3a42fca013b306c4abb0b8
arindam-modak <arindam.modak03@gmail.com>
2018-12-13 17:01:43 +0530

    react app

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My React App</h1>
+        <p>Welcome hehe</p>
+      </div>
+    );
+  }
+}
+
+export default App;
